12021f1d201c275547de39778e0fc69e1d348b8b
karthik karunakaran <karthikk0424@gmail.com>
2015-06-23 00:12:04 -0400

    Summary: Add admin controller

---
+++ Controller/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibraryInventorySystem.Books;
+
+namespace LibraryInventorySystem.Controller
+{
+    class AdminController
+    {
+        public static void AwaitingApprovals()
+        {
+            Book.ListAllAwaitingApprovals();
+        }
+    }
+}
