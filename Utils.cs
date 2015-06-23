ce3adbcef574add669144b079fff3c5ae9915391
karthik karunakaran <karthikk0424@gmail.com>
2015-06-23 00:10:48 -0400

    Summary: Table implementation

--- Utils.cs
+++ Utils.cs
@@ -24,14 +24,42 @@ namespace LibraryInventorySystem
             } while (!valid);
 
             return selection;
-        } 
-
-        public static XmlDocument LoadedDocument()
-        {
-            XmlDocument document =  new XmlDocument();
-            document.Load(Constants.BOOKS_XML_FILE);
-            return document;
-
         }
+
+        #region TableHelper by Patrick McDonald
+        private static int tableWidth = 110;
+
+        private static void PrintLine()
+        {
+            Console.WriteLine(new string('-', tableWidth));
+        }
+
+        public static void PrintRow(params string[] columns)
+        {
+            int width = (tableWidth - columns.Length) / columns.Length;
+            string row = "";
+
+            foreach (string column in columns)
+            {
+                row += AlignCentre(column, width) + "";
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine(row);
+        }
+
+        private static string AlignCentre(string text, int width)
+        {
+            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+            }
+        }
+        #endregion
     }
 }
